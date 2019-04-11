1efad4ed37caba01734e413623186245867f46af
blackavgn <svid.egor@gmail.com>
2019-04-11 19:10:51 +0300

    beutify

--- src/main/java/com/leverx/springapp/demo/DemoApplication.java
+++ src/main/java/com/leverx/springapp/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.leverx.springapp.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     @Value("${keyword.first}")
     private String keywordOne;
+
     @Value("${keyword.second}")
-    private String keywordTwo;
+      private String keywordTwo;
 
     @RequestMapping("/")
-    public String home(){
-      return keywordOne+keywordTwo;
+    public String home() { 
+        return keywordOne + keywordTwo;
     }
 
     public static void main(String[] args) {
